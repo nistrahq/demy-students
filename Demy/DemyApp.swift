b9a85a98c7b86c9be84afccb994dea13b2bd670e
Salim Ramirez <salimramirezm@gmail.com>
2025-09-13 09:31:54 -0500

    Initial Commit

---
+++ Demy/DemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemyApp.swift
+//  Demy
+//
+//  Created by Alumnos on 13/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
