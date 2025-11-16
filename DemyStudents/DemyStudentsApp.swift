5554388a0e23c9d2b4d1089373091a95ac155576
Salim Ramirez <salimramirezm@gmail.com>
2025-11-16 14:16:43 -0500

    chore: new initial iOS project structure + gitignore + gitattributes

---
+++ DemyStudents/DemyStudentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemyStudentsApp.swift
+//  DemyStudents
+//
+//  Created by Salim Ramirez Mestanza on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DemyStudentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
