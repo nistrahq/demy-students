ad2422dc335135464e62c20d632135759d126f1b
Salim Ramirez <salimramirezm@gmail.com>
2025-11-22 18:04:35 -0500

    feat(auth): add user entity and authentication repository with sign-in use case

---
+++ DemyStudents/Features/Auth/Domain/Entities/User.swift
@@ -0,0 +1,11 @@
+//
+//  User.swift
+//  DemyStudents
+//
+//  Created by Salim Ramirez Mestanza on 22/11/25.
+//
+
+struct User: Identifiable, Codable, Equatable {
+    let id: String
+    let email: String
+}
