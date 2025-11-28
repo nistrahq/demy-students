0713014b3041fdc28c6c2a78af687e8f454bb71e
iconicmiau <u202224135@gmail.com>
2025-11-28 02:03:29 -0500

    fix: profile view AGAINn

--- DemyStudents/App/Config/Environment.swift
+++ DemyStudents/App/Config/Environment.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Environment {
     static let apiBaseURL: String = {
         #if DEBUG
-        return "http://127.0.0.1:8080/api/v1"
+        return "https://nistra-demy.up.railway.app/api/v1"
         #else
         return "https://nistra-demy.up.railway.app/api/v1"
         #endif
